b5e32971c947d9e091c0d799c8fe2808a8245b41
Caleb Wang <wang10@cooper.edu>
2012-10-26 01:27:11 -0400

    2nd Python Excercise

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-This is a coding exercise I am doing for the Hackerati. The program NFLstats.py sets up a local
-server that takes a current NFL players name (ex: Peyton Manning) then retrieves player stats and
-displays it for the user.
+This is a coding exercise I am doing for the Hackerati. The program googleAPI.py sets up a local
+server that takes a users google shopping search and displays the first 25 items with their prices
+in a table.
 
 To use this code make sure the following Python modules are installed:
 - HTML.py which can be installed from http://www.decalage.info/python/html
@@ -8,7 +8,7 @@ To use this code make sure the following Python modules are installed:
 
 You can check to see what python modules are installed by typing help('modules') in a python shell
 
-Run the program by typing "python NFLstats.py" (no quotes) in the command line
+Run the program by typing "python googleAPI.py" (no quotes) in the command line
 while in the directory with all of the files.
 
 The following text should be displayed:
@@ -18,4 +18,4 @@ Hit Ctrl-C to quit.
 
 Using a web browser, go to: http://localhost:8080/prompt
 
-From this webpage you can enter NFL players names and retrieve data.
+From this webpage you can enter products and retrieve data.
--- googleAPI.py
+++ googleAPI.py
@@ -1,36 +1,67 @@
-import os
-import sys
 import urllib2
-from bottle import route, run, get, post, request
 import HTML
-
-url = "https://www.googleapis.com/shopping/search/v1/public/products?key=AIzaSyDYyZCC32BnqauGRprJlB1c4fPSpx6Wym0&country=US&q=digital+camera&alt=json"
-key = "AIzaSyDYyZCC32BnqauGRprJlB1c4fPSpx6Wym0"
-output = urllib2.urlopen(url).read()
-jsonData = json.loads(output)
-print output
-
+from bottle import route, run, get, post, request
+#import HTML
 
 # The prompt page where it asks for a players name
 @route('/prompt')
 def hello():
-    # Read in the html file from the local directory
-    f = open('prompt.htm')
-    return f.read()
-    f.close()
+	# Read in the html file from the local directory
+	f = open('prompt.htm')
+	return f.read()
+	f.close()
 
 
 # The page where it displays the data
 @post('/data')
 def login_submit():
-    # Replace spaces with + signs to insert into the url
-    search = request.forms.get('name').replace(' ','+')
-    url = "https://www.googleapis.com/shopping/search/v1/public/products?key=AIzaSyDYyZCC32BnqauGRprJlB1c4fPSpx6Wym0&country=US&q="+search+"&alt=json"
-    output = urllib2.urlopen(url).read()
-    print url
+	# Replace spaces with + signs to insert into the url
+	search = request.forms.get('name').replace(' ','+')
+	url = "https://www.googleapis.com/shopping/search/v1/public/products?key=AIzaSyDYyZCC32BnqauGRprJlB1c4fPSpx6Wym0&country=US&q="+search+"&alt=json"
+	output = urllib2.urlopen(url).readlines()
+	# Initialize the table
+	table = [ ['Product Name', 'Price'] ]
+	# parse the output
+	# Look through each line for the data I want
+	for index in range(len(output)):
+		line = output[index]
+		# Check for height data
+		if line.count('"title":') > 0:
+			title = getTitle(line)
+		if line.count('"price":') > 0:
+			price = getPrice(line)
+			temp = [title, price]
+			table.append(temp)
+		# Package the data
+	print table
+	# Turn the python list into an html text table
+	htmlTable = HTML.table(table)
+	outFile1 = open('output1.htm')
+	outFile2 = open('output2.htm')
+	return outFile1.read() + htmlTable + outFile2.read()
+	outFile1.close()
+	outFile2.close()
 
+
+# get title function
+def getTitle(line):
+	line = line.strip()
+	temp = line.split(' ',1)[1]
+	temp = temp.strip(',')
+	temp = temp.strip('"')
+	return temp
+
+# get price function
+def getPrice(line):
+	line = line.strip()
+	temp = line.split(' ',1)[1]
+	temp = temp.strip(',')
+	temp = temp.strip('"')
+	temp = '$'+temp
+	if temp[-2] == '.':
+		temp = temp + '0'
+	return temp
 
 
 # Setup the local server
 run(host='localhost', port=8080, debug=True)
-
